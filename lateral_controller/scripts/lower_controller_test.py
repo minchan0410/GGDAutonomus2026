3a4d8ace69f34100cf606fcc4e9f00ce54572711
JihunByonAJOU <josephx03021@ajou.ac.kr>
2026-01-04 22:22:32 +0900

    로워 컨트롤러에 임의 des_steer 값 집어넣는 검증용 테스트 코드

---
+++ lateral_controller/scripts/lower_controller_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import rospy
+import math
+from std_msgs.msg import Int16
+
+class Test:
+    def __init__(self):
+        self.motor_cmd_steer_pub = rospy.Publisher(
+            "/des_steer", Int16, queue_size=1
+        )
+
+        self.rate = rospy.Rate(20)  # 20 Hz
+        self.start_time = rospy.Time.now().to_sec()
+
+        self.run()
+
+    def run(self):
+        while not rospy.is_shutdown():
+            t = rospy.Time.now().to_sec() - self.start_time
+
+            # sin 파 생성: -30 ~ +30
+            steer_value = int(30 * math.sin(2 * math.pi * 0.2 * t))
+            # ↑ 0.2 Hz = 5초에 한 번 왕복 (원하면 바꿔도 됨)
+
+            msg = Int16()
+            msg.data = steer_value
+
+            self.motor_cmd_steer_pub.publish(msg)
+            self.rate.sleep()
+
+
+if __name__ == "__main__":
+    rospy.init_node("test")
+    Test()
